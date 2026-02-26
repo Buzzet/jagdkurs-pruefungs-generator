416584f121a5ba6af5e82ce1228e8a3fe6566825
Jannik Reinefeld <jannik@dreigl.de>
2026-02-26 14:40:07 +0100

    fix: remove white viewport edge by setting global html/body background and reset margins

--- app.vue
+++ app.vue
@@ -1,3 +1,16 @@
 <template>
   <NuxtPage />
 </template>
+
+<style>
+html, body, #__nuxt {
+  margin: 0;
+  padding: 0;
+  min-height: 100%;
+  background: #090b10;
+}
+
+* {
+  box-sizing: border-box;
+}
+</style>
